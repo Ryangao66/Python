de3692df8bc070baaf877daff8ef24202958c1d5
ryangao66 <ryangao66@gmail.com>
2024-08-06 15:54:37 +1200

    update

---
+++ Python_Crash_Course/Chapter 3 列表简介/3.3 管理列表/3.3.2_sort临时排序.py
@@ -0,0 +1,17 @@
+# 创建一个列表[], 然后用sorted(var)进行临时排序
+# Owner: Ryan
+# Date: 06-Aug-2024
+
+cars = ["bmw", "audi", "toyota", "subaru"]
+print("Here's the original list:")
+print(cars)
+
+print("\nHere's the sorted list:")
+print(sorted(cars))
+
+print("\nHere's the reversed sorted list:")  #
+print(sorted(cars))  #
+
+
+print("\nHere's the original list again:")
+print(cars)
