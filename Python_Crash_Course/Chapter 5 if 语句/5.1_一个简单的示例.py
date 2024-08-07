4a29f0e181f797b6a9c232c6afecde0faf1910be
ryangao66 <ryangao66@gmail.com>
2024-08-07 21:06:57 +1200

    update

---
+++ Python_Crash_Course/Chapter 5 if 语句/5.1_一个简单的示例.py
@@ -0,0 +1,9 @@
+# Owner: Ryan
+# Date: 07-Aug-2024
+
+cars = ["audi", "bmw", "subaru", "toyota"]
+for car in cars:
+    if car == "bmw":
+        print(car.upper())
+    else:
+        print(car.lower())
